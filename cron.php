ac14258261df2106241e5381e24727cd9b358df6
ubuntu <math.face.15@gmail.com>
2025-07-09 17:56:50 +0530

    login started

---
+++ cron.php
@@ -0,0 +1,4 @@
+<?php
+
+file_put_contents('cronlog.txt', time()."\n", FILE_APPEND);
+?>
---
+++ signup.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'libs/load.php';
+
+?>
+
+<!doctype html>
+<html lang="en">
+<?php load_template('_head'); ?>
+
+<body>
+
+    <?php load_template('_header'); ?>
+    <main>
+
+        <?php load_template('_signup'); ?>
+
+    </main>
+    <?php load_template('_footer'); ?>
+    <script src="<?php get_config('base_path')?>assets/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
