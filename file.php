962d3da73cfc34068d55e3a3a7bb98987b137486
ubuntu <math.face.15@gmail.com>
2025-08-15 14:09:07 +0530

    setup image in memory cache and .htaccess configration

--- file.php
+++ file.php
@@ -11,13 +11,13 @@ $image_path = str_replace('..', '', $image_path);
 
 if (is_file($image_path)) {
     //TODO: Lot of security things to think about here
-    header("Content-Type:".mime_content_type($image_path));
+   header("Content-Type:".mime_content_type($image_path));
     header("Content-Length:".filesize($image_path));
-     header('Cache-control: max-age='.(60*60*24*365));
+    header('Cache-control: max-age='.(60*60*24*365));
     header('Expires: '.gmdate(DATE_RFC1123, time()+60*60*24*365));
-    header('Last-Modified: '.gmdate(DATE_RFC1123, filemtime($image_path)));
+    header('Last-Modified: '.gmdate(DATE_RFC1123, filemtime($path_to_image)));
     header_remove('Pragma');
-
+    
     echo file_get_contents($image_path);
 }
 else {
